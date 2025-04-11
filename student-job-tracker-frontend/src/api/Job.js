09369cde712a707dadc349baaaf6dd3b9de3818b
Suraj kumar <sooraj4475@gmail.com>
2025-04-11 22:01:19 +0530

    Update Job.js

--- student-job-tracker-frontend/src/api/Job.js
+++ student-job-tracker-frontend/src/api/Job.js
@@ -5,9 +5,9 @@ const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
 
 export const getJobs = () => axios.get(`${API_BASE}/api/jobs`);
 
-export const addJob = (jobData) => axios.post(`${API_BASE}/jobs`, jobData);
+export const addJob = (jobData) => axios.post(`${API_BASE}/api/jobs`, jobData);
 
 export const updateJobStatus = (id, status) =>
   axios.patch(`${API_BASE}/jobs/${id}`, { status });
 
-export const deleteJob = (id) => axios.delete(`${API_BASE}/jobs/${id}`);
+export const deleteJob = (id) => axios.delete(`${API_BASE}/api/jobs/${id}`);
