d8d215c7008199812eca86af7985328c156c6377
Suraj kumar <sooraj4475@gmail.com>
2025-04-11 21:27:59 +0530

    Update Job.js

--- student-job-tracker-frontend/src/api/Job.js
+++ student-job-tracker-frontend/src/api/Job.js
@@ -1,7 +1,7 @@
 // src/api/jobs.js
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+const API_BASE = import.meta.env.VITE_API_BASE/api || "http://localhost:5000/api";
 
 export const getJobs = () => axios.get(`${API_BASE}/jobs`);
 
